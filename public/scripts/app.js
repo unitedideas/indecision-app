061ceb9c3df483b7d9aef58e42ad547e5889cb1c
Unitedideas <unitedideas@gmail.com>
2019-03-06 19:25:07 -0800

    class basesd components (state) vs function based components (no state tracking)

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -62,13 +62,12 @@ var IndecisionApp = function (_React$Component) {
     }, {
         key: 'render',
         value: function render() {
-            var title = 'Indecision';
             var subtitle = 'Put your life in the hands of a computer';
 
             return React.createElement(
                 'div',
                 null,
-                React.createElement(Header, { title: title, subtitle: subtitle }),
+                React.createElement(Header, { subtitle: subtitle }),
                 React.createElement(Action, {
                     hasOptions: this.state.options.length > 0,
                     handlePick: this.handlePick
@@ -96,12 +95,17 @@ var Header = function Header(props) {
             null,
             props.title
         ),
-        React.createElement(
+        props.subtitle && React.createElement(
             'h2',
             null,
             props.subtitle
         )
     );
+};
+
+Header.defaultProps = {
+    title: 'Indecision'
+
 };
 
 var Action = function Action(props) {
--- src/app.js
+++ src/app.js
@@ -41,12 +41,11 @@ class IndecisionApp extends React.Component {
     }
 
     render() {
-        const title = 'Indecision';
         const subtitle = 'Put your life in the hands of a computer';
 
         return (
             <div>
-                <Header title={title} subtitle={subtitle}/>
+                <Header subtitle =  {subtitle} />
                 <Action
                     hasOptions={this.state.options.length > 0}
                     handlePick={this.handlePick}
@@ -68,9 +67,15 @@ const Header = (props) => {
     return (
         <div>
             <h1>{props.title}</h1>
-            <h2>{props.subtitle}</h2>
+            {props.subtitle && <h2>{props.subtitle}</h2>
+            }
         </div>
     );
+};
+
+Header.defaultProps = {
+    title: 'Indecision',
+
 };
 
 
