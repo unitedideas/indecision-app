3caf9331c432d9fea950753a43ea6b7115472bee
Unitedideas <unitedideas@gmail.com>
2019-03-25 10:15:31 -0700

    jsx import / export

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 
-import './utils.js'
+// import './utils.js';
+import {square, add} from './utils.js'
 
 
 console.log('App.js is running!!');
-
+console.log (square(4));
+console.log (add(4, 10));
--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,7 @@
 console.log('utils.js is running!!')
+
+const square = (x) => x * x;
+
+const add = (x, y) => x + y;
+
+export { square, add };
