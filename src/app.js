2a75e31a4f05647e8c2b9fa05132ddb21c259777
Unitedideas <unitedideas@gmail.com>
2019-03-28 10:24:54 -0700

    inports and exports

--- src/app.js
+++ src/app.js
@@ -1,23 +1,3 @@
-// import './utils.js';
-import subtract, {
-    square,
-    add,
+// install -- import -- use
+import validator from 'validator';
 
-} from './utils.js';
-
-import isSenior, {
-    isAdult,
-    canDrink,
-} from './person.js';
-
-
-
-console.log('App.js is running!!');
-console.log(square(4));
-console.log(add(4, 10));
-console.log(subtract(100, 50));
-console.log(isSenior(64))
-
-
-// console.log(isAdult(18), isAdult(17))
-// console.log(canDrink(21), canDrink(20))
