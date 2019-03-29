7e1be14ea6033029858a4257bdd12a27b0eb1f02
Unitedideas <unitedideas@gmail.com>
2019-03-28 17:58:21 -0700

    babel / webpack setup

--- src/app.js
+++ src/app.js
@@ -3,6 +3,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const template = <p>This is JSX</p>
+const template = <p>This is JSX for Webpack</p>
 ReactDOM.render(template, document.getElementById('app'))
 
